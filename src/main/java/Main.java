a694505e6b49f8d8bbdc2e00446e64e40885e6d3
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-03-19 17:38:47 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,6 +68,8 @@ public class Main {
                           clientSocket.write(responseMessage);
                       }
                   }
+
+                  iterator.remove();
               }
 
           }
