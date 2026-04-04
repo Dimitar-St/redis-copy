3372937ce0224540df48d2f108a32a35b26a3dd6
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-04-04 11:48:18 +0300

    codecrafters submit [skip ci]

--- src/main/java/ParserFactory.java
+++ src/main/java/ParserFactory.java
@@ -16,7 +16,7 @@ public class ParserFactory {
         byte firstByte = payload.get(0);
         IParser parser = parsers.get((int) firstByte);
         System.out.println(parser);
-        System.out.println(firstByte);
+        System.out.println(Character.valueOf((char) firstByte));
         System.out.println(String.valueOf(payload.array()));
 
         if (parser == null) {
