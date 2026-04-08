65488cb524faaecd6c757fa71ecafb1589f9878e
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-04-08 18:16:54 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/Lpush.java
+++ src/main/java/commands/Lpush.java
@@ -41,6 +41,7 @@ public class Lpush implements ICommand {
     private void insertElements(String[] elements, List<String> list) {
         int length = elements.length-1;
         for (int i = length; i >= 0; i--) {
+            System.out.println(elements[i]);
            list.add(0, elements[i]);
         }
     }
