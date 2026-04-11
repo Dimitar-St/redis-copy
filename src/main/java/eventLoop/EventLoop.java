265c78f4c1c5dd7700096eb07c47176419ccc59c
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-04-11 19:15:55 +0300

    codecrafters submit [skip ci]

--- src/main/java/eventLoop/EventLoop.java
+++ src/main/java/eventLoop/EventLoop.java
@@ -104,6 +104,7 @@ public class EventLoop {
 
                             if (waitingCommand.isBlocking()) {
                                 if (response2.equals("not present")) {
+                                    System.out.println("No data present yet.");
                                     return;
                                 }
                             }
