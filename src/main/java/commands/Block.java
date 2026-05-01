612dd043a92aac5717ca4401465c7cb0fd161e3a
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-05-01 10:59:05 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/Block.java
+++ src/main/java/commands/Block.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Block {
-    private ArrayList<String> fields;
+    private List<String> fields;
 
     public Block() {}
 
     public void setFields(List<String> fields) {
-       this.fields = (ArrayList<String>) fields;
+       this.fields =  fields;
     }
 
     public void appendField(List<String> fields) {
