664b4ea7496d6035e9f95ed28422be6f2685feba
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-05-08 17:58:11 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/Xread.java
+++ src/main/java/commands/Xread.java
@@ -74,8 +74,8 @@ public class Xread extends BaseCommand {
             SortedMap<StreamID, Block> map;
             if (isBlocking) {
                 map = store.getAfter(pair.streamID);
+                System.out.println("waiting");
                 if (map.isEmpty()) {
-                    System.out.println("waiting");
                     this.blockingManager.addClient(this, "not present", this.connection, this.selectionKey);
                     return "not present";
                 }
