53ea99e6c49dbbfcf91ea2a42d4723383629732a
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-05-09 11:59:55 +0300

    codecrafters submit [skip ci]

--- src/main/java/eventLoop/BlockingClientManager.java
+++ src/main/java/eventLoop/BlockingClientManager.java
@@ -34,7 +34,7 @@ public class BlockingClientManager {
         System.out.println("Clients waiting: " + clients.size());
         System.out.println("timeout: " + clients.peek().command.timeout);
         System.out.println("now: " + now);
-        while (!clients.isEmpty() && clients.peek().command.timeout <= now) {
+        while (!clients.isEmpty() && clients.peek().command.timeout <= System.currentTimeMillis()) {
             WaitingClient client = clients.poll();
 
             if (client.completed) {
