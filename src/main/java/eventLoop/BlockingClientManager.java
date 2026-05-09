cb2c9ae42d9f85835675ac42d6c3a3d675b6a090
Dimitar-St <stoyanov.dimi.tri@gmail.com>
2026-05-09 11:39:33 +0300

    codecrafters submit [skip ci]

--- src/main/java/eventLoop/BlockingClientManager.java
+++ src/main/java/eventLoop/BlockingClientManager.java
@@ -30,6 +30,7 @@ public class BlockingClientManager {
             return;
         }
 
+        System.out.println("Handle timeout connections...");
         while (!clients.isEmpty() && clients.peek().command.timeout <= now) {
             WaitingClient client = clients.poll();
 
